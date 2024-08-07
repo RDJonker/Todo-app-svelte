e65f58ff850140663f9e21b5c84c2e07dbf66c64
RDJonker <105273376+RDJonker@users.noreply.github.com>
2024-08-07 06:01:46 +0200

    Add files via upload

---
+++ src/components/ClearTodos.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    type clearCompletedType = () => void
+    type CompletedTodos = number
+
+    export let clearCompleted: clearCompletedType
+    export let completedTodos: CompletedTodos
+</script>
+
+<button 
+on:click={clearCompleted}
+    class:hidden={completedTodos === 0}
+    class="clear-completed"
+    >
+    Clear completed
+</button>
